b1e942ccc8178ecbca34fa509ff9a0a41fa074d4
George Ponick <gponick@gmail.com>
2018-05-25 21:44:21 -0400

    More better error handling and cleanup.

--- BTMLColorLOSMod/SettingsColorHelper.cs
+++ BTMLColorLOSMod/SettingsColorHelper.cs
@@ -12,14 +12,15 @@ namespace BTMLColorLOSMod
         // and use that.
         public static Color ColorFromValues(float r, float g, float b, float a)
         {
-            float outR, outG, outB, outA;
-            outR = rgbaColorValueFromFloat(r);
-            outG = rgbaColorValueFromFloat(g);
-            outB = rgbaColorValueFromFloat(b);
-            outA = rgbaColorValueFromFloat(a);
+            float outR=-1, outG=-1, outB=-1, outA=-1;
+       
 
             try
             {
+                outR = rgbaColorValueFromFloat(r);
+                outG = rgbaColorValueFromFloat(g);
+                outB = rgbaColorValueFromFloat(b);
+                outA = rgbaColorValueFromFloat(a);
                 Color outColor = new Color(outR, outG, outB, outA);
                 return outColor;
             }
@@ -36,16 +37,11 @@ namespace BTMLColorLOSMod
         // if the value is X > 1, we'll divide it by 255 then return
         public static float rgbaColorValueFromFloat(float value)
         {
-            float outValue;
-            if(value !=0 && value > 1)
-            {
-                outValue = (float)(value / 255);
-            }
-            else
-            {
-                outValue = value;
-            }
-            return outValue;
+            if (value < 0 || value > 255)
+                throw new ArgumentOutOfRangeException(string.Format("value is out of range: {0}", value));
+            if (value <= 1)
+                return value;
+            return value / 255f;
         }
     }
 }
